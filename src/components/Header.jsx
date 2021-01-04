cf9d9bbd7820476e047641bf353da27801d9db38
roberto <robpalacios1@hotmail.com>
2021-01-04 13:24:02 -0500

    import Icons from material-ui

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 
+/***** Icons *****/
+import PersonIcon from '@material-ui/icons/Person';
+import IconButton from '@material-ui/core/IconButton'
+import ForumIcon from '@material-ui/icons/Forum';
+
 /** CSS */
 import './Header.css'
 
 const Header = () => {
     return (
         <div className="header">
-            <h2>Im a Header</h2>
+            <IconButton>
+                <PersonIcon fontSize="large" className="header__icon" />
+            </IconButton>
+
+            <img
+                className="header__logo"
+                src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png"
+                alt=""
+            />
+
+            <IconButton>
+                <ForumIcon fontSize="large" className="header__icon" />
+            </IconButton>
         </div>
     )
 }
