5fbdf0bbc51b2192c74327ba0a247330f1ae64f8
roberto <robpalacios1@hotmail.com>
2021-01-04 16:42:38 -0500

    add useState with Api Object

--- src/components/TinderCards.jsx
+++ src/components/TinderCards.jsx
@@ -1,12 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+/***** TinderCard *****/
+import TinderCard from "react-tinder-card"
 
 /***** CSS  *****/
 import "./TinderCards.css"
 
 const Tindercards = () => {
+
+    const [people, setPeople] = useState([
+        {
+            name: "Elon Musk",
+            url: "https://www.biography.com/.image/t_share/MTY2MzU3Nzk2OTM2MjMwNTkx/elon_musk_royal_society.jpg",
+        },
+
+        {
+            name: "Jeff Bezos",
+            url: "https://biography.com/.image/t_share/MTY2NzA3ODE3OTgwMzcyMjYw/jeff-bezos-andrew-harrer_bloomberg-via-getty-images.jpg",
+
+        },
+
+        {
+            name: "Sandra Bullock",
+            url: "https://m.media-amazon.com/images/M/MV5BMTI5NDY5NjU3NF5BMl5BanBnXkFtZTcwMzQ0MTMyMw@@._V1_UY1200_CR94,0,630,1200_AL_.jpg",
+        }
+    ])
+
+    const swiped = (direction, nameToDelete) => {
+        console.log("removing " + nameToDelete);
+        //setLastDirection(direction);
+    }
+
+    const outOfFrame = (name) => {
+        console.log(name + " left the screen")
+    }
+
     return (
         <div className="tinderCards">
-            <h2>iam the card</h2>
+            <div className="tinderCards__cardContainer">
+            {
+                people.map((person) => (
+                    <TinderCard
+                        className="swipe"
+                        key={person.name}
+                        preventSwipe={["up", "down"]}
+                        onSwipe={(dir) => swiped(dir, person.name)}
+                        onCardLeftScreen={() => outOfFrame(person.name)}
+                    >
+                        <div
+                            style={{backgroundImage: `url(${person.url})`}}
+                            className="card"
+                        >
+                            <h3>{person.name}</h3>
+                        </div>
+                    </TinderCard>
+                ))
+            }
+            </div>
         </div>
     )
 }
