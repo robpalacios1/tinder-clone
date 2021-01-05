8a83e381e6a3b03617aeefe675fec26be8507a56
roberto <robpalacios1@hotmail.com>
2021-01-05 15:56:37 -0500

    add useEffect

--- src/components/TinderCards.jsx
+++ src/components/TinderCards.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from '../axios';
 
 /***** TinderCard *****/
 import TinderCard from "react-tinder-card"
@@ -10,21 +11,29 @@ const Tindercards = () => {
 
     const [people, setPeople] = useState([
         {
-            name: "Elon Musk",
-            url: "https://www.biography.com/.image/t_share/MTY2MzU3Nzk2OTM2MjMwNTkx/elon_musk_royal_society.jpg",
+            "name": "Elon Musk",
+            "imgUrl": "https://www.biography.com/.image/t_share/MTY2MzU3Nzk2OTM2MjMwNTkx/elon_musk_royal_society.jpg",
         },
 
         {
-            name: "Jeff Bezos",
-            url: "https://biography.com/.image/t_share/MTY2NzA3ODE3OTgwMzcyMjYw/jeff-bezos-andrew-harrer_bloomberg-via-getty-images.jpg",
-
+            "name": "Jeff Bezos",
+            "imgUrl": "https://biography.com/.image/t_share/MTY2NzA3ODE3OTgwMzcyMjYw/jeff-bezos-andrew-harrer_bloomberg-via-getty-images.jpg",
         },
 
         {
-            name: "Sandra Bullock",
-            url: "https://m.media-amazon.com/images/M/MV5BMTI5NDY5NjU3NF5BMl5BanBnXkFtZTcwMzQ0MTMyMw@@._V1_UY1200_CR94,0,630,1200_AL_.jpg",
-        }
+            "name": "Sandra Bullock",
+            "imgUrl": "https://m.media-amazon.com/images/M/MV5BMTI5NDY5NjU3NF5BMl5BanBnXkFtZTcwMzQ0MTMyMw@@._V1_UY1200_CR94,0,630,1200_AL_.jpg",
+        },
     ])
+
+    useEffect(() => {
+        async function fetchData() {
+            const req = await axios.get("/tinder/cards")
+            setPeople(req.data);
+        }
+        fetchData();
+    }, [])
+    console.log(people);
 
     const swiped = (direction, nameToDelete) => {
         console.log("removing " + nameToDelete);
@@ -48,7 +57,7 @@ const Tindercards = () => {
                         onCardLeftScreen={() => outOfFrame(person.name)}
                     >
                         <div
-                            style={{backgroundImage: `url(${person.url})`}}
+                            style={{backgroundImage: `url(${person.imgUrl})`}}
                             className="card"
                         >
                             <h3>{person.name}</h3>
